8b7149ce2a155af3a455a58f2cb36ac872ba04d9
sara <paranoiaque17@gmail.com>
2022-10-27 12:57:21 +0900

    first test

---
+++ src/components/WrapComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const WrapComponent = () => {
+    return (
+        <div>
+            <p>WrapComponent입니다</p>
+        </div>
+    );
+};
+
+export default WrapComponent;
