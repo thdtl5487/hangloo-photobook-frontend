778801976a6d7efe068ca12c447938056ba275c5
thdtl5487 <thdtl5487@gmail.com>
2022-11-04 16:43:07 +0900

    [추가] jQuery 모듈 추가 main에서 테스트중인 코드 작성 (머지 시 삭제 필요)

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,13 +1,72 @@
 import axios from 'axios';
 //import React, { Component, useState } from 'react';
 import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { json, Link } from 'react-router-dom';
 import 'animate.css';
+import pickNote from '../dummydb/data/dummy_PickNote.json';
+import pickNotePhoto from '../dummydb/data/dummy_PicNotePhoto.json';
+import $ from 'jquery';
 
 
 const MainComponent = () => {
+
+    const makeJsonTest = (e)=>{
+        const getNoteNumList = [];
+        for(var i = 0; i<pickNote.length; i++){
+            getNoteNumList.push(pickNote[i].noteNum);
+        }
+
+        console.log(getNoteNumList)
+
+        const uniqueNoteNumList = [];
+
+        getNoteNumList.forEach((element)=>{
+            if(!uniqueNoteNumList.includes(element)){
+                uniqueNoteNumList.push(element);
+            }
+        })
+
+        console.log(uniqueNoteNumList);
+        const notePicData = [];
+        for(var i = 0; i<uniqueNoteNumList.length; i++){
+            notePicData[i] = uniqueNoteNumList[i];
+            notePicData[i] = [];
+            for(var j = 0; j<pickNotePhoto.length; j++){
+                console.log("uNum : " + uniqueNoteNumList[i])
+                if(uniqueNoteNumList[i] == pickNotePhoto[j].noteNum){
+                    
+                    notePicData[i].push(pickNotePhoto[j]);
+                }
+            }
+        }
+
+        console.log(notePicData);
+
+        var jsonData = JSON.stringify(notePicData);
+        console.log(jsonData);
+
+        $.ajax({
+            type:"POST",
+            url:'/jsonTest',
+            data: JSON.stringify({
+                jsonData: pickNotePhoto
+                }),
+            contentType: 'application/json',
+            dataType: 'json',
+        }).done(function(res){
+            if(res.statusCode === 200){
+                alert("보내기 성공");
+            }else{
+                alert("오류 발생");
+            }
+        })
+
+
+    }
+
     return (       
         <div id="main">
+            {makeJsonTest()}
             <div className="wrap">
                 <ul>
                     <li>
