4b3afc159a9831499671619738e937adb1657d23
merry <sne0525@naver.com>
2022-11-07 13:25:29 +0900

    옵션선택페이지(반응형적용)

--- src/components/SelectDetailOptionComponent.js
+++ src/components/SelectDetailOptionComponent.js
@@ -28,13 +28,18 @@ const SelectDetailOption = () => {
     choicelist:[]
   });
 
+  const [checkNoteText, setCheckNoteText] = useState('선택');
+  const [checkAlbumText, setCheckAlbumText] = useState('선택');
+
   const onNote = () =>{
     let imsi=[];
     if(choice.choicelist.includes("note")===false){
       setChoice({...choice, choicelist:[...choice.choicelist, "note"]})
+	  setCheckNoteText(<>선택</>)
     }else{
      imsi = choice.choicelist.filter((item)=>item !== "note");
      setChoice({...choice, choicelist:imsi})
+	 setCheckNoteText(<>해제</>)
     }
     console.log(choice)
   }
@@ -43,9 +48,11 @@ const SelectDetailOption = () => {
     let imsi=[];
     if(choice.choicelist.includes("album")===false){
       setChoice({...choice, choicelist:[...choice.choicelist, "album"]})
+	  setCheckAlbumText(<>선택</>)
     }else{
      imsi = choice.choicelist.filter((item)=>item !== "album");
      setChoice({...choice, choicelist:imsi})
+	 setCheckAlbumText(<>해제</>)
     }
     console.log(choice)
   }
@@ -53,32 +60,98 @@ const SelectDetailOption = () => {
     const [noteCheckList, setNoteCheckList] = useState({
       noteCheckElement:['check1', 'check2','check3']
     });
+	
+
+	// const textChocie = "선택";
+	// const textCancle = "제외";
+
+	const [check1Text, setCheck1Text] = useState('제외');
+	const [check2Text, setCheck2Text] = useState('제외');
+	const [check3Text, setCheck3Text] = useState('제외');
+	const [check4Text, setCheck4Text] = useState('제외');
+	const [check5Text, setCheck5Text] = useState('제외');
+
+	const textCheck1Change = (e) => {
+		if(e.target.checked===false){
+			console.log("text"+ e.target.checked)
+			setCheck1Text(<>선택</>)
+		}else{
+			
+			setCheck1Text(<>제외</>)
+		}
+
+	}
+
+	const textCheck2Change = (e) => {
+		if(e.target.checked===false){
+			console.log("text"+ e.target.checked)
+			setCheck2Text(<>선택</>)
+		}else{
+			
+			setCheck2Text(<>제외</>)
+		}
+
+	}
+
+	const textCheck3Change = (e) => {
+		if(e.target.checked===false){
+			console.log("text"+ e.target.checked)
+			setCheck3Text(<>선택</>)
+		}else{
+			
+			setCheck3Text(<>제외</>)
+		}
+
+	}
+	
+	const textCheck4Change = (e) => {
+		if(e.target.checked===false){
+			console.log("text"+ e.target.checked)
+			setCheck4Text(<>선택</>)
+		}else{
+			
+			setCheck4Text(<>제외</>)
+		}
+
+	}
+
+	const textCheck5Change = (e) => {
+		if(e.target.checked===false){
+			console.log("text"+ e.target.checked)
+			setCheck5Text(<>선택</>)
+		}else{
+			
+			setCheck5Text(<>제외</>)
+		}
+
+	}
+
 
     const onNoteChange = (e) => {
-      let imsi=[];
-      console.log("checked" + e.target)
-      console.log(e);
-      if(e.target.checked===true){
-        setNoteCheckList({...noteCheckList, noteCheckElement:[...noteCheckList.noteCheckElement, e.target.value]})
-      }else{
-        imsi = noteCheckList.noteCheckElement.filter((item)=>item !== e.target.value);
-        setNoteCheckList({...noteCheckList, noteCheckElement:imsi});
-      }
-      console.log(noteCheckList)
-      console.log(choice)
+		let imsi=[];
+		console.log("checked" + e.target)
+		console.log(e);
+		if(e.target.checked===true){
+			setNoteCheckList({...noteCheckList, noteCheckElement:[...noteCheckList.noteCheckElement, e.target.value]})
+		}else{
+			imsi = noteCheckList.noteCheckElement.filter((item)=>item !== e.target.value);
+			setNoteCheckList({...noteCheckList, noteCheckElement:imsi});
+		}
+		console.log(noteCheckList)
+		console.log(choice)
     }
+
+
     const [content, setContent] = useState();
     
-    const onClick = (e) => {
-
-      console.log(e)
+    const onImgClick = (e) => {
+      	console.log(e)
         // e.preventDefault();
       // console.log(e.target.value.substring(5))
       // console.log(img[e.target.value.substring(5)-1].id)
 
-      const idx = e.target.value.substring(5)-1;
-      setContent(<img className="sub" src={img[idx].image} alt={img[idx].title}></img>);
-
+      	const idx = e.target.value.substring(5)-1;
+      	setContent(<img className="sub" src={img[idx].image} alt={img[idx].title}></img>);
     }
 
     const [albumCheckList, setAlbumCheckList] = useState({
@@ -86,7 +159,6 @@ const SelectDetailOption = () => {
     })
 
     const onAlbumChange = (e) => {
-      console.log("tttt")
       let imsi=[];
       if(e.target.checked===true){
         setAlbumCheckList({...albumCheckList, albumCheckElement:[...albumCheckList.albumCheckElement, e.target.value]})
@@ -139,7 +211,7 @@ const SelectDetailOption = () => {
 									<label className="note" htmlFor="note-check">
 								*/}
 								<Accordion.Header onClick={onNote}>
-									알림장 선택
+									알림장 {checkNoteText}
 								</Accordion.Header>
 									
 
@@ -147,26 +219,31 @@ const SelectDetailOption = () => {
 									<input type="checkbox" className="btn-check" id="btn-check-2-outlined" value="check1"
 									onChange = {onNoteChange}
 									checked = {noteCheckList.noteCheckElement.includes('check1')}
-									onClick = {onClick}>
+									onClick = {(e) => {onImgClick(e);
+														textCheck1Change(e);}}>
 									</input>
 									<label className="btn btn-outline-secondary" htmlFor="btn-check-2-outlined"> 
-									사진 없는 알림장 제외
+										사진 없는 알림장 {check1Text}
 									</label>
 
 									<input type="checkbox" className="btn-check" id="btn-check-2-outlined2" value="check2"
 									onChange = {onNoteChange}
 									checked = {noteCheckList.noteCheckElement.includes('check2')}
-									onClick = {onClick}>
+									onClick = {(e) => {onImgClick(e);
+														textCheck2Change(e);}}>
 									</input>
-									<label className="btn btn-outline-secondary" htmlFor="btn-check-2-outlined2">내용 없는 알림장 제외
+									<label className="btn btn-outline-secondary" htmlFor="btn-check-2-outlined2">
+										내용 없는 알림장 {check2Text} 
 									</label>
 
 									<input type="checkbox" className="btn-check" id="btn-check-2-outlined3" value="check3"
 									onChange = {onNoteChange}
 									checked = {noteCheckList.noteCheckElement.includes('check3')}
-									onClick = {onClick}>
+									onClick = {(e) => {onImgClick(e);
+														textCheck3Change(e);}}>
 									</input>
-									<label className="btn btn-outline-secondary" htmlFor="btn-check-2-outlined3">가정에서 보낸 알림장 제외
+									<label className="btn btn-outline-secondary" htmlFor="btn-check-2-outlined3">
+										가정에서 보낸 알림장 {check3Text}
 									</label>
 
 								</Accordion.Body>
@@ -178,7 +255,7 @@ const SelectDetailOption = () => {
 							<Accordion>
 								<Accordion.Item eventKey="1">
 								<Accordion.Header onClick={onAlbum}>
-									사진앨범 선택
+									사진앨범 {checkAlbumText}
 								</Accordion.Header>
 								
 								<Accordion.Body>
@@ -186,17 +263,21 @@ const SelectDetailOption = () => {
 									<input type="checkbox" className="btn-check" id="btn-check-2-outlined4" value="check4"
 									onChange = {onAlbumChange}
 									checked = {albumCheckList.albumCheckElement.includes('check4')}
-									onClick = {onClick}>
+									onClick = {(e) => {onImgClick(e);
+														textCheck4Change(e);}}>
 									</input>
-									<label className="btn btn-outline-secondary" htmlFor="btn-check-2-outlined4">내용 없는 앨범 제외
+									<label className="btn btn-outline-secondary" htmlFor="btn-check-2-outlined4">
+										내용 없는 앨범 {check4Text}
 									</label>
 
 									<input type="checkbox" className="btn-check" id="btn-check-2-outlined5" value="check5"
 									onChange = {onAlbumChange}
 									checked = {albumCheckList.albumCheckElement.includes('check5')}
-									onClick = {onClick}>
+									onClick = {(e) => {onImgClick(e);
+														textCheck5Change(e);}}>
 									</input>
-									<label className="btn btn-outline-secondary" htmlFor="btn-check-2-outlined5">댓글 없는 앨범 제외
+									<label className="btn btn-outline-secondary" htmlFor="btn-check-2-outlined5">
+										댓글 없는 앨범 {check5Text}
 									</label>
 
 
