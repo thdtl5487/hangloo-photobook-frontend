8dc0b5beb4be8f48d05a62c3362e6dc5753a413b
thdtl5487 <thdtl5487@gmail.com>
2022-11-11 09:32:25 +0900

    [추가] api 적용, 프록시 2곳으로 분리

---
+++ src/apiutil/apiloader.js
@@ -0,0 +1,154 @@
+import axios from 'axios';
+
+class apiloader{
+
+    // 로그인 기능
+    loginTest(id, pw){
+        axios({
+            url: "/photobook/api/certificate.php",
+            method: "POST",
+            headers: {
+                "dataType": 'json',
+                "Accept": "application/json, text/javascript, */*; q=0.01",
+                "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"
+            },
+            data: {
+                id:id,
+                password:pw
+            }
+        }).then((res) =>{
+            console.log("API의 로그인 기능 실행");
+            console.log(res);
+            return res;
+        })
+    }
+
+    // 접속한 계정의 아동 정보 불러오기
+    getChildTest = () =>{
+        axios({
+            url: "/photobook/api/children.php",
+            method: "GET",
+            // headers: {
+            //     "Content-Type": 'application/json'
+            // },
+        }).then((res)=>{
+            console.log("API의 아동 정보 불러오기 기능 실행");
+            const resultList = {
+                result : []
+            }
+            var resData = {
+                uid : "",
+                name : "",
+                birthday : "",
+                photo: "",
+            }
+
+            for(var i = 0; i < res.data.list.length; i++){
+                resData = {
+                    uid : res.data.list[i].child_uid,
+                    name : res.data.list[i].name,
+                    birthday : res.data.list[i].birthday,
+                    photo : res.data.list[i].photo
+                }
+
+                resultList.result.push(resData);
+            }
+
+            console.log(resultList)
+            return resultList;
+        })
+    }
+
+    // 접속한 계정의 게시글 개수 불러오기
+    getPostsCount = (startDate, endDate, childNum) => {
+        axios({
+            url: "/photobook/api/posts_count.php?start_date="+startDate+"&end_date="+endDate+"&s_child="+childNum,
+            method: "GET",
+            headers: {
+                "Accept": "application/json, text/javascript, */*; q=0.01",
+            },
+        }).then((res) => {
+            console.log("API의 포토앨범 개수 불러오기 기능 실행, 조회 아동번호 : " + childNum)
+            console.log(res);
+        })
+    }
+
+    // 접속한 계정의 게시글 정보 불러오기 (페이지, 검색시작날짜, 검색종료날짜, 아동번호)
+    getPosts = (page, startDate, endDate, childNum) => {
+        axios({
+            url: "/photobook/api/notices.php?page="+page+"start_date="+startDate+"&end_date="+endDate+"&s_child="+childNum,
+            method: "GET",
+            headers: {
+                "Content-Type": 'application/json'
+            },
+        }).then((res) =>{
+            console.log(res);
+        })
+    }
+
+    // 접속한 계정의 알림장 개수 불러오기
+    getNoticesCount = () => {
+        axios({
+            url: "/photobook/api/notices_count.php",
+            method: "GET",
+            headers: {
+                "Content-Type": 'application/json'
+            },
+        }).then((res) => {
+            console.log(res);
+        })
+    }
+
+    // 접속한 계정의 알림장 정보 불러오기
+    getNotices = (page, startDate, endDate, childNum) => {
+        axios({
+            url: "/photobook/api/notices.php?page="+page+"start_date="+startDate+"&end_date="+endDate+"&s_child="+childNum,
+            method: "GET",
+            headers: {
+                "Content-Type": 'application/json'
+            },
+        }).then((res)=>{
+            console.log(res);
+        })
+    }
+
+    // 접속한 계정의 댓글 개수 불러오기
+    getCommentCount = (startDate, endDate, childNum) =>{
+        axios({
+            url: "/photobook/api/comments.php?start_date="+startDate+"&end_date="+endDate+"&s_child="+childNum,
+            method: "GET",
+            headers: {
+                "Content-Type": 'application/json'
+            }
+        }).then((res)=>{
+            console.log(res);
+        })
+    }
+
+    // 접속한 계정의 댓글 정보 불러오기
+    getComment = (page, startDate, endDate, childNum) =>{
+        axios({
+            url: "/photobook/api/comments.php?page="+page+"start_date="+startDate+"&end_date="+endDate+"&s_child="+childNum,
+            method: "GET",
+            headers: {
+                "Content-Type": 'application/json'
+            },
+        }).then((res)=>{
+            console.log(res);
+        })
+    }
+
+    // 게시글의 사진 리스트 불러오기
+    getPhoto = (comm_uid, type) =>{
+        axios({
+            url: "/photobook/api/photos.php?comm_uid="+comm_uid+"&type="+type,
+            method: "GET",
+            headers: {
+                "Content-Type": 'application/json'
+            }        
+        }).then((res)=>{
+            console.log(res);
+        })
+    }
+}
+export default new apiloader()
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,8 +1,10 @@
 
+import { type } from '@testing-library/user-event/dist/type';
 import axios from 'axios';
 //import React, { Component, useState } from 'react';
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import apiloader from '../apiutil/apiloader';
 
 
 
@@ -18,8 +20,8 @@ const MainComponent = () => {
     const getNoticeInfoTest = () =>{
         console.log("getNoticeInfoTest 실행")
         axios({
-            url: "/getAllNoticesInfo",
-            method: "GET"
+            url: "/photobookServer/getAllNoticesInfo",
+            method: "GET",
         })
         .then((res)=>{
             
@@ -36,7 +38,8 @@ const MainComponent = () => {
 
             testData.data = [{
                 aaa: "zzzz",
-                bbb: "dddd"
+                bbb: "dddd",
+                ccc: "wwww"
             }]
 
             console.log(testData);
@@ -63,8 +66,11 @@ const MainComponent = () => {
         })
 
         axios({
-            url: "/sendPhotoAlbumInfo",
+            url: "/photobookServer/sendPhotoAlbumInfo",
             method:"POST",
+            // headers:{
+            //     "Content-Type" : 'application/json'
+            // },
             data: currentYearNotices.noticeList
         }).then((res)=>{
             console.log("전송완료");
@@ -76,7 +82,7 @@ const MainComponent = () => {
     const getNoticePicUrl = (noticeNum) =>{
 
         axios({
-            url: "/getPhotoToNoticeNum",
+            url: "/photobookServer/getPhotoToNoticeNum",
             method: "POST",
             data: {
                 noticeNum: 530
@@ -86,7 +92,23 @@ const MainComponent = () => {
             console.log(res);
         })
 
+
+        axios.post()
     }
+
+    const [testValue, setTestValue] = useState(
+        {
+            id : 'test924',
+            pw : 'good',
+            start_date : 20200101,
+            end_data : 20221231,
+            child_pid : 4892,
+            select_page : 2,
+            comm_uid : 12345,
+            type : 'note'
+        }
+    )
+
 
     return (       
         <div id="main">
@@ -94,6 +116,16 @@ const MainComponent = () => {
 
                 <button onClick={getNoticeInfoTest}>알림장 정보 전체 가져오기 버튼</button>
                 <button onClick={sendPhotoAlbumData}>사용할 알림장 정보 보내기</button>
+                <button onClick={getNoticePicUrl}>dddddd</button>
+                <button onClick={()=>apiloader.loginTest(testValue.id, testValue.pw)}>로그인 테스트</button>
+                <button onClick={()=>apiloader.getChildTest()}>아이있는지 테스트</button>
+                <button onClick={()=>apiloader.getPostsCount(testValue.start_date, testValue.end_data, testValue.child_pid)}>게시물개수 테스트</button>
+                <button onClick={()=>apiloader.getPosts(testValue.select_page, testValue.start_date, testValue.end_data, testValue.child_pid)}>게시물 가져오기 테스트</button>
+                <button onClick={()=>apiloader.getNoticesCount()}>알림장 개수 테스트</button>
+                <button onClick={()=>apiloader.getNotices(testValue.select_page, testValue.start_date, testValue.end_data, testValue.child_pid)}>알림장 가져오기 테스트</button>
+                <button onClick={()=>apiloader.getCommentCount(testValue.start_date, testValue.end_data, testValue.child_pid)}>댓글 개수 테스트</button>
+                <button onClick={()=>apiloader.getComment(testValue.select_page, testValue.start_date, testValue.end_data, testValue.child_pid)}>댓글 가져오기 테스트</button>
+                <button onClick={()=>apiloader.getPhoto(testValue.comm_uid, testValue.type)}>사진 가져오기 테스트</button>
                 <Link to="/SelectThemeComponent">포토북 만들기</Link>
 
             </div>
