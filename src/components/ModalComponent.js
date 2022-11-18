6021d76f839f619fe373493465a058a906d55986
sara <paranoiaque17@gmail.com>
2022-11-18 13:16:49 +0900

    이미지출력이안되고있습니다

--- src/components/ModalComponent.js
+++ src/components/ModalComponent.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import React, {useEffect, useRef} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import kids2 from '../images/kidsdummy2.jpeg';
 import kids3 from '../images/kidsdummy3.jpeg';
 
@@ -23,7 +23,76 @@ const ModalComponent = ({modal, setModal, modalCloseFn, monthData}) => {
     const reciveDataTest= ()=>{
         console.log(monthData)
     }
+    const [question, setQuestion] = useState(
+        {
+            question:[]
+        }
+    )
   
+    const axiosFn=()=>{
+        //let tempuid = list.linkUrl.split('?id=note&type=view&uid=').slice(1);
+        let tempuid = [];
+        if(monthData.monthData.length >= 1){
+            for(let i=0; i<monthData.monthData.length; i++){
+                tempuid.push(monthData.monthData[i].linkUrl.split('?id=note&type=view&uid=').slice(1));
+            }
+        }
+
+        //let result = '';
+        console.log('tempuid', tempuid);
+        tempuid.map(()=>{
+            axios({
+                url: "/photobook/api/photos.php?comm_uid="+tempuid+"&type=note",
+                method: "GET",
+                headers: {  
+                    "Content-Type": 'application/json'
+                }        
+            }).then((res)=>{
+                    console.log('959641', res.data.list.photo_list);
+                    setQuestion({...question, question:[res.data.list.photo_list]})
+                }
+            )
+        })
+    }
+    useEffect(()=>{
+        axiosFn();
+    },[monthData])
+
+    const photoList = question.question.map((question,index)=>{
+        return (
+            <li className="photo-frame" key={index}>
+            <div className="photo-frame-gap">
+                <div className="photo-frame-wrap">
+                    <label htmlFor={index}>
+                        <input type="checkbox" id={index} name={index} value="a"/>
+                        {/* {axiosFn()} */}
+                        <img src={question} alt={question} />
+                        {/* <img src={printNotePicture(list.linkUrl.split('?id=note&type=view&uid='))} alt="" /> */}
+                        {/* {printNotePicture(list.linkUrl.split('?id=note&type=view&uid='))}
+                        {/* <img src={"/photobook/api/photos.php?comm_uid="+list.linkUrl.split('?id=note&type=view&uid=').slice(1)+"&type=note"} alt="" />
+                        {list.linkUrl.split('?id=note&type=view&uid=').slice(1)} */}
+                    </label>
+                </div>
+            </div>
+        </li>
+        )
+    })
+    //const test = (e) => {
+        //console.log('작동하는지 테스트', monthData.monthData[0]);
+        // let tempuid = monthData.monthData[0].linkUrl.split('?id=note&type=view&uid=').slice(1);
+        // axios({
+        //     url: "/photobook/api/photos.php?comm_uid="+tempuid+"&type=note",
+        //     method: "GET",
+        //     headers: {
+        //         "Content-Type": 'application/json'
+        //     }        
+        // }).then((res)=>{
+        //     console.log(res.data);
+        // })
+    //}
+    // useEffect(()=>{
+    //     test();
+    // },[monthData])
     
 
     // const photoList = (e) =>  {
@@ -44,38 +113,42 @@ const ModalComponent = ({modal, setModal, modalCloseFn, monthData}) => {
     //     }    
     // }
 
-    const printNotePicture = (uid) =>{
-        var tempuid = uid.slice(1);
-        var result;
-        axios({
-            url: "/photobook/api/photos.php?comm_uid="+tempuid+"&type=note",
-            method: "GET",
-            headers: {
-                "Content-Type": 'application/json'
-            }        
-        }).then((res)=>{
-            // if(res.data.list.photo_list===undefined){
-            //     return;
-            // }
-            // else {
-                //let result = res.data.list.photo_list;
-                console.log(res.data.list.photo_list[0]);
-                //return (
-                //    result
-                //)
-                result   = res.data.list.photo_list[0];
-                console.log("result : ", result)
-                
-            }
-            
-        //}
-        )
-        console.log("result out axios : ",result);
-        return (
-            <img src={result} alt="zz" />
-        )
+    // const printNotePicture = (uid) =>{
+    //     var tempuid = uid.slice(1);
+    //     var result;
+    //     const axiosFn=()=>{
+    //         axios({
+    //             url: "/photobook/api/photos.php?comm_uid="+tempuid+"&type=note",
+    //             method: "GET",
+    //             headers: {
+    //                 "Content-Type": 'application/json'
+    //             }        
+    //         }).then((res)=>{
+    //             // if(res.data.list.photo_list===undefined){
+    //             //     return;
+    //             // }
+    //             // else {
+    //                 //let result = res.data.list.photo_list;
+    //                 console.log(res.data.list.photo_list[0]);
+    //                 //return (
+    //                 //    result
+    //                 //)
+    //                 result = res.data.list.photo_list[0];
+    //                 setQuestion({...question, question:res.data.list.photo_list[0]});
+    //             //}
+    //             }
+    //         )
+    //         console.log("result : ", result);
+    //     }
+    //     axiosFn();
+    //     console.log('resultresult', result);
+
+    // //     console.log("result out axios : ",result);
+    // //     return (
+    // //         <img src={result} alt="zz" />
+    // //     )
         
-    }
+    // }
 
 
     // const photoList = monthData.monthData.map((photoList,index)=>{    
@@ -116,7 +189,12 @@ const ModalComponent = ({modal, setModal, modalCloseFn, monthData}) => {
     //     console.log(photolist);
     // })
 
-    
+    // const photoList = question.question.map(question=>{
+    //     return (
+    //     <img src={question} alt={question} />
+    //     )
+    // })
+
     const list = monthData.monthData.map((list,index)=>{    
         return (
             <li className="photo-date" key={index}>
@@ -124,17 +202,7 @@ const ModalComponent = ({modal, setModal, modalCloseFn, monthData}) => {
                     <p>{list.dateWeek}</p>
                 </div>
                 <ul className="photo">
-                    <li className="photo-frame">
-                        <div className="photo-frame-gap">
-                            <div className="photo-frame-wrap">
-                                <label htmlFor={index}>
-                                    <input type="checkbox" id={index} name={index} value="a"/>
-                                    {/* <img src={printNotePicture(list.linkUrl.split('?id=note&type=view&uid='))} alt="zz" /> */}
-                                    {printNotePicture(list.linkUrl.split('?id=note&type=view&uid='))}
-                                </label>
-                            </div>
-                        </div>
-                    </li>
+                    {photoList}
                 </ul>
             </li>
         )
